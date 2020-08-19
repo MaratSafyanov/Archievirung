72588c019b0e43d93a9609446e1433c5612963fd
Marat Safyanov <marat.safyanov@ges.thm.de>
2020-08-19 16:02:07 +0200

    added navbar on homepage

--- templates/home.php
+++ templates/home.php
@@ -10,20 +10,35 @@
 </head>
 <body>
 
-<ul class="nav nav-tabs ">
-    <li class="nav-item">
-        <a class="nav-link" href="#">Ordner anlegen</a>
-    </li>
-    <li class="nav-item">
-    <a class="nav-link" href="#">Ordner Archivieren</a>
-    </li>
-    <li class="nav-item">
-    <a class="nav-link" href="#">Container suchen</a>
-    </li>
-    <li class="nav-item">
-    <a class="nav-link" href="#">Ordner suchen</a>
-    </li>
-</ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-success" style="background-color:#2ed151 ; color: #ffffff">
 
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="ordner_anlegen.php">Ordner anlegen <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="container_anlegen.php">Container anlegen</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="gitter_box_list.php">Container list</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="ordner_list.php">Ordner list</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="ordner_archivieren.php">Ordner zuordnen</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="ordner_suchen.php">Ordner suchen</a>
+            </li>
+
+        </ul>
+    </div>
+
+</nav>
+<div class="container">
+
+</div>
 </body>
 </html>
