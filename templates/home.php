819272df7a0ca734ce10cb420df2e34942309157
Marat Safyanov <marat.safyanov@ges.thm.de>
2020-08-27 12:49:19 +0200

    added login function

--- templates/home.php
+++ templates/home.php
@@ -1,10 +1,18 @@
-<?php include $_SERVER['DOCUMENT_ROOT']."/templates/includes/header.php"; ?>
-<?php include $_SERVER['DOCUMENT_ROOT']."/templates/includes/navbar.php"; ?>
-
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/templates/includes/header.php"; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/templates/includes/navbar.php"; ?>
 
 
 <div class="container">
+<br>
 
+    <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+            <div class='alert alert-info'>Sie haben erfolgreich angemeldet!</div>
+
+        </div>
+        <div class="col-sm-3"></div>
+    </div>
 </div>
 </body>
 </html>
--- templates/login.php
+++ templates/login.php
@@ -1,4 +1,53 @@
-<?php ?>
+<?php
+//
+include_once "../src/connect.php";
+Db::initialize();
+?>
+
+<?php
+
+    if (isset($_POST['login'])) {
+
+            $connection = Db::$conn;
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $username = mysqli_real_escape_string($connection, $username);
+            $password = mysqli_real_escape_string($connection, $password);
+
+            $query = "SELECT * FROM accounts WHERE username = '$username' ";
+            $select_user_query = mysqli_query($connection, $query);
+
+                        if (!$select_user_query) {
+                            die("Login Query failed" . mysqli_error($connection));
+                        }
+
+                        while ($row = mysqli_fetch_array($select_user_query)) {
+
+
+                            $db_id = $row['id'];
+                            $db_username = $row['username'];
+                            $db_password = $row['passwort'];
+
+                            if ($password === $db_password) {
+                                $_SESSION['username'] = $db_username;
+                                $_SESSION['passwort'] = $db_password;
+                                header("Location:  home.php");
+                            }else{
+
+                                header("Location login.php");
+
+
+                            }
+
+                        }
+
+
+
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
@@ -11,24 +60,29 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-sm"></div>
+        <div class="col-sm-4"></div>
 
-        <div class="col-sm">
-            <form class="form-signin">
+        <div class="col-sm-4">
+            <form class="form-signin" action="" method="post">
                 <img class="mb-4" src="" alt="" width="72" height="72">
-                <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-                <label for="username" class="sr-only">Email address</label>
+                <h1 class="h3 mb-3 font-weight-normal">Bitte sign in</h1>
+                <label for="username" class="sr-only">Username</label>
                 <input type="text" id="username" class="form-control" placeholder="Username" required=""
-                       autofocus="">
+                       autofocus="" name="username">
                 <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+                <br>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required=""
+                       name="password">
 
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                <hr>
+
+                <input class="btn btn-lg btn-success btn-block" type="submit" name="login" value="Sign in">
 
             </form>
+
         </div>
 
-        <div class="col-sm"></div>
+        <div class="col-sm-4"></div>
     </div>
 
 </div>
